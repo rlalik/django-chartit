cba6e6f96c44132e0348c1136ea91e125e336616
Javier Wilson <javier.wilson@gmail.com>
2020-02-05 13:16:49 -0600

    tolerate None in charts

--- chartit/templatetags/chartit.py
+++ chartit/templatetags/chartit.py
@@ -80,8 +80,11 @@ def load_charts(chart_list=None, render_to=''):
             chart_list = [chart_list]
         chart_list = [c.hcoptions for c in chart_list]
         render_to_list = [s.strip() for s in render_to.split(',')]
+
         for hco, render_to in six.moves.zip_longest(
                 chart_list, render_to_list):
+            if hco is None:
+                break
             if render_to:
                 hco['chart']['renderTo'] = render_to
         embed_script = (embed_script % (json.dumps(chart_list,
